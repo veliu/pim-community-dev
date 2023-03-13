52143d0a18ba776f8282eee420a97eb7e742deb0
Celine Morin <celine.morin@akeneo.com>
2023-03-13 15:39:25 +0100

    CPM-951: Revert akademy menu

--- front-packages/shared/src/components/Navigation/PimNavigation.unit.tsx
+++ front-packages/shared/src/components/Navigation/PimNavigation.unit.tsx
@@ -6,54 +6,45 @@ import {aMainNavigation} from './navigationTestHelper';
 
 jest.mock('../PimView');
 
-beforeAll(() => {
-  // @ts-ignore
-  global.fetch = () =>
-    Promise.resolve({
-      ok: true,
-      status: 200,
-      json: () => Promise.resolve({pim_version: 'serenity'}),
-    });
-});
 test('It displays the main navigation with an active entry and a sub menu', async () => {
-  renderWithProviders(
-    <PimNavigation entries={aMainNavigation()} activeEntryCode="entry1" activeSubEntryCode="subentry2" />
-  );
+    renderWithProviders(
+        <PimNavigation entries={aMainNavigation()} activeEntryCode="entry1" activeSubEntryCode="subentry2" />
+    );
 
-  expect(screen.getByLabelText('Main navigation')).toBeInTheDocument();
-  expect(screen.getByTestId('pim-sub-menu')).toBeInTheDocument();
-  expect(screen.queryAllByTestId('pim-main-menu-item')).toHaveLength(3);
-  expect(screen.queryAllByTestId('locked-entry')).toHaveLength(0);
+    expect(screen.getByLabelText('Main navigation')).toBeInTheDocument();
+    expect(screen.getByTestId('pim-sub-menu')).toBeInTheDocument();
+    expect(screen.queryAllByTestId('pim-main-menu-item')).toHaveLength(3);
+    expect(screen.queryAllByTestId('locked-entry')).toHaveLength(0);
 });
 
 test('It displays the main navigation with an active entry without a sub menu', async () => {
-  renderWithProviders(<PimNavigation entries={aMainNavigation()} activeEntryCode="entry2" activeSubEntryCode={null} />);
+    renderWithProviders(<PimNavigation entries={aMainNavigation()} activeEntryCode="entry2" activeSubEntryCode={null} />);
 
-  expect(screen.queryAllByTestId('pim-main-menu-item')).toHaveLength(3);
-  expect(screen.getByLabelText('Main navigation')).toBeInTheDocument();
-  expect(screen.queryByTestId('pim-sub-menu')).toBeFalsy();
+    expect(screen.queryAllByTestId('pim-main-menu-item')).toHaveLength(3);
+    expect(screen.getByLabelText('Main navigation')).toBeInTheDocument();
+    expect(screen.queryByTestId('pim-sub-menu')).toBeFalsy();
 });
 
 test('It redirects the user to the clicked entry route', async () => {
-  renderWithProviders(
-    <PimNavigation entries={aMainNavigation()} activeEntryCode="entry1" activeSubEntryCode="subentry2" />
-  );
+    renderWithProviders(
+        <PimNavigation entries={aMainNavigation()} activeEntryCode="entry1" activeSubEntryCode="subentry2" />
+    );
 
-  mockedDependencies.router.redirect = jest.fn();
-  fireEvent.click(screen.getByText('Entry 1'));
-  expect(mockedDependencies.router.redirect).toHaveBeenCalledWith('entry1_route');
+    mockedDependencies.router.redirect = jest.fn();
+    fireEvent.click(screen.getByText('Entry 1'));
+    expect(mockedDependencies.router.redirect).toHaveBeenCalledWith('entry1_route');
 });
 
 test('It show disabled entries as locked for the Free Trial', async () => {
-  renderWithProviders(
-    <PimNavigation
-      entries={aMainNavigation()}
-      activeEntryCode="entry10"
-      activeSubEntryCode="subentry2"
-      freeTrialEnabled={true}
-    />
-  );
+    renderWithProviders(
+        <PimNavigation
+            entries={aMainNavigation()}
+            activeEntryCode="entry10"
+            activeSubEntryCode="subentry2"
+            freeTrialEnabled={true}
+        />
+    );
 
-  expect(screen.queryAllByTestId('pim-main-menu-item')).toHaveLength(3);
-  expect(screen.queryAllByTestId('locked-entry')).toHaveLength(1);
+    expect(screen.queryAllByTestId('pim-main-menu-item')).toHaveLength(3);
+    expect(screen.queryAllByTestId('locked-entry')).toHaveLength(1);
 });
--- front-packages/shared/src/components/PimView.tsx
+++ front-packages/shared/src/components/PimView.tsx
@@ -1,46 +1,185 @@
-import React, {useRef, useState, useEffect} from 'react';
-import styled from 'styled-components';
-import {useIsMounted, useViewBuilder} from '../hooks';
-import {View} from '../DependenciesProvider.type';
+import React, {FC, useMemo} from 'react';
+import styled, {css} from 'styled-components';
+import {PimView} from '../PimView';
+import {useRouter, useTranslate} from '../../hooks';
+import {IconProps, LockIcon, MainNavigationItem, Tag, useTheme} from 'akeneo-design-system';
+import {SubNavigation, SubNavigationEntry, SubNavigationType} from './SubNavigation';
+import {useAnalytics} from '../../hooks';
 
-type Props = {
-  viewName: string;
-  className?: string;
-};
+const StyledMainNavigationItem = styled(MainNavigationItem)<{align?: 'bottom'; freeTrialEnabled: boolean}>`
+  ${({align}) =>
+  align === 'bottom' &&
+  css`
+      position: absolute;
+      bottom: 0;
+    `}
 
-const StyledPimView = styled.div<{rendered: boolean}>`
-  visibility: ${({rendered}) => (rendered ? 'visible' : 'hidden')};
-  opacity: ${({rendered}) => (rendered ? '1' : '0')};
-  transition: opacity 0.5s linear;
+  ${({disabled, freeTrialEnabled}) =>
+  disabled &&
+  freeTrialEnabled &&
+  css`
+      cursor: pointer;
+    `}
 `;
 
-const PimView = ({viewName, className}: Props) => {
-  const el = useRef<HTMLDivElement>(null);
-  const [view, setView] = useState<View | null>(null);
+type NavigationEntry = {
+  code: string;
+  title: string;
+  disabled?: boolean;
+  route: string;
+  icon: React.ReactElement<IconProps>;
+  subNavigations: SubNavigationType[];
+  isLandingSectionPage: boolean;
+  align?: 'bottom';
+};
 
-  const viewBuilder = useViewBuilder();
-  const isMounted = useIsMounted();
-  useEffect(() => {
-    if (!viewBuilder) {
+type Props = {
+  entries: NavigationEntry[];
+  activeEntryCode: string | null;
+  activeSubEntryCode: string | null;
+  freeTrialEnabled?: boolean;
+};
+
+const PimNavigation: FC<Props> = ({entries, activeEntryCode, activeSubEntryCode, freeTrialEnabled = false}) => {
+  const translate = useTranslate();
+  const router = useRouter();
+  const theme = useTheme();
+  const analytics = useAnalytics();
+
+  const handleFollowEntry = (event: any, entry: NavigationEntry) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    analytics.track('navigation:entry:clicked', {
+      code: entry.code,
+    });
+
+    router.redirect(router.generate(entry.route));
+  };
+
+  const activeNavigationEntry = useMemo((): NavigationEntry | undefined => {
+    return entries.find((entry: NavigationEntry) => entry.code === activeEntryCode);
+  }, [entries, activeEntryCode]);
+
+  const activeSubNavigation = useMemo((): SubNavigationType | undefined => {
+    if (undefined === activeNavigationEntry) {
       return;
     }
 
-    viewBuilder.build(viewName).then(view => {
-      if (isMounted()) {
-        view.setElement(el.current).render();
-        setView(view);
-      }
+    return activeNavigationEntry.subNavigations.find((column: SubNavigationType) => {
+      return undefined !== column.entries.find((entry: SubNavigationEntry) => entry.code === activeSubEntryCode);
     });
-  }, [viewBuilder, viewName]);
+  }, [activeNavigationEntry, activeSubEntryCode]);
 
-  useEffect(
-    () => () => {
-      view && view.remove();
-    },
-    [view]
+  return (
+    <NavContainer aria-label="Main navigation">
+      <MainNavContainer>
+        <LogoContainer>
+          <PimView viewName="pim-menu-logo" />
+        </LogoContainer>
+        <MenuContainer>
+          {entries.map(entry => (
+            <StyledMainNavigationItem
+              id={entry.code}
+              key={entry.code}
+              active={entry.code === activeEntryCode}
+              disabled={entry.disabled}
+              icon={entry.icon}
+              onClick={event => handleFollowEntry(event, entry)}
+              href={`#${router.generate(entry.route)}`}
+              role="menuitem"
+              data-testid="pim-main-menu-item"
+              className={entry.code === activeEntryCode ? 'active' : undefined}
+              align={entry.align}
+              freeTrialEnabled={freeTrialEnabled}
+            >
+              {translate(entry.title)}
+              {entry.disabled && freeTrialEnabled && (
+                <LockIconContainer data-testid="locked-entry">
+                  <StyledTag tint="blue">
+                    <StyledLockIcon size={16} color={theme.color.blue100} />
+                  </StyledTag>
+                </LockIconContainer>
+              )}
+            </StyledMainNavigationItem>
+          ))}
+        </MenuContainer>
+        <HelpContainer>
+          <PimView viewName="pim-menu-help" />
+        </HelpContainer>
+      </MainNavContainer>
+      {activeNavigationEntry &&
+        (!activeNavigationEntry.isLandingSectionPage || activeSubEntryCode) &&
+        activeSubNavigation &&
+        activeSubNavigation.sections.length > 0 && (
+          <SubNavigation
+            entries={activeSubNavigation.entries}
+            sections={activeSubNavigation.sections}
+            backLink={activeSubNavigation.backLink}
+            stateCode={activeSubNavigation.stateCode}
+            title={activeSubNavigation.title}
+            activeSubEntryCode={activeSubEntryCode}
+            freeTrialEnabled={freeTrialEnabled}
+          />
+        )}
+    </NavContainer>
   );
-
-  return <StyledPimView className={className} ref={el} rendered={null !== view} />;
 };
 
-export {PimView};
+const StyledTag = styled(Tag)`
+  padding: 0;
+  height: 24px;
+  width: 24px;
+`;
+
+const StyledLockIcon = styled(LockIcon)`
+  margin: 3px;
+`;
+
+const LockIconContainer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 12px;
+  width: 24px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NavContainer = styled.nav`
+  display: flex;
+  height: 100%;
+`;
+
+const MainNavContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: start;
+  height: 100%;
+  border-right: 1px solid ${({theme}) => theme.color.grey80};
+  z-index: 803;
+  background: white;
+  overflow: auto;
+`;
+
+const LogoContainer = styled.div`
+  height: 80px;
+  min-height: 80px;
+  position: relative;
+`;
+
+const MenuContainer = styled.div`
+  position: relative;
+  height: 100%;
+`;
+const HelpContainer = styled.div`
+  height: 80px;
+  min-height: 80px;
+  position: relative;
+  margin-top: auto;
+`;
+
+export type {NavigationEntry, SubNavigation};
+export {PimNavigation};
